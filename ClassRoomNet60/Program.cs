6e0a4cb5ed9bd420cfae0ac735dd5873d3582826
Sebastian Brinch <sebastianbrinch.mail@gmail.com>
2024-08-27 13:59:15 +0200

    Done with task 3.

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassRoomNet60;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClassRoom classRoom = new ClassRoom();
+        
+        classRoom.ClassName = "3Q";
+        classRoom.SemesterStart = DateTime.Today;
+        
+        Student mathias = new Student("Mathias", 8, 11);
+        Student sebastian = new Student("Sebastian", 4, 15);
+        
+        classRoom.StudentList.Add(mathias);
+        classRoom.StudentList.Add(sebastian);
+
+        foreach (var s in classRoom.StudentList)
+        {
+            Console.WriteLine(s.Name);
+        }
+    }
+}
