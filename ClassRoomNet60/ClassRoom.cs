26951d6f8ffffc2080e2125d46b89193fdec1ca7
Sebastian Brinch <sebastianbrinch.mail@gmail.com>
2024-08-27 13:47:19 +0200

    Created new class ClassRoom and created the different ClassRoom properties.

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,13 @@
+namespace ClassRoomNet60;
+
+public class ClassRoom
+{
+    public string ClassName { get; set; }
+    public List<Student> StudentList { get; set; }
+    public DateTime SemesterStart { get; set; }
+
+    public ClassRoom()
+    {
+        
+    }
+}
